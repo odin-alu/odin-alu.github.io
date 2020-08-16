988255248903c36aaae3f51fbda43915c9d288ef
JulianMason <JulianMason@users.noreply.github.com>
2020-08-16 19:25:34 +0100

    weatherApp

--- Main/assets/js/weather.js
+++ Main/assets/js/weather.js
@@ -1,13 +1,13 @@
 function load(){
 	var city = document.getElementById("city").value;
-	urlAPI = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&appid=663718f5418b05dca23eb3d1d5fe3a67';
+	urlAPI = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&appid=663718f5418b05dca23eb3d1d5fe3a67';
 	const form = document.querySelector(".about form");
 	const input = document.querySelector(".about input");
 	const msg = document.querySelector(".about .msg");
-	const list = document.querySelector(".about #day");
+	const list = document.querySelector(".ajax-section .cities");
 
 	//Check if there is already a city 
-	const listItems = list.querySelectorAll(".about .city");
+	const listItems = list.querySelectorAll(".ajax-section .cities");
 	const listItemsArray = Array.from(listItems); 
 
 
@@ -48,13 +48,6 @@ function load(){
 			var dayDesc = data.list[0].weather[0].description;
 			var feelsLike = data.list[0].main.feels_like.toFixed(0);
 
-			$('#date').html('Current Weather');
-			$('#city-name').append(cityName + ', ' + country);
-			$('#desc').append(dayDesc);
-			$('#temp').html(dayTemp + '<sup>°c</sup>');
-			$('#icon').empty().append(image);
-			$('#feels').html('Feels like: ' + feelsLike + '<sup>°c</sup>');
-
 			
 			/*Day 1 Forecast*/
 			var dayTemp1 = data.list[8].main.temp.toFixed(0);
@@ -62,23 +55,11 @@ function load(){
 			var feelsLike1 = data.list[8].main.feels_like.toFixed(0);
 			var date1 = data.list[8].dt_txt;
 
-			$('#date1').html(date1);
-			$('#desc1').append(dayDesc1);
-			$('#temp1').html(dayTemp1 + '<sup>°c</sup>');
-			$('#icon1').empty().append(image1);
-			$('#feels1').html('Feels like: ' + feelsLike1 + '<sup>°c</sup>');
-
 			/*Day 2 Forecast*/
 			var dayTemp2 = data.list[16].main.temp.toFixed(0);
 			var dayDesc2 = data.list[16].weather[0].description;
 			var feelsLike2 = data.list[16].main.feels_like.toFixed(0);
 			var date2 = data.list[16].dt_txt;
-
-			$('#date2').html(date2);
-			$('#desc2').append(dayDesc2);
-			$('#temp2').html(dayTemp2 + '<sup>°c</sup>');
-			$('#icon2').empty().append(image2);
-			$('#feels2').html('Feels like: ' + feelsLike2 + '<sup>°c</sup>');
 
 
 			/*Day 3 Forecast*/
@@ -87,12 +68,6 @@ function load(){
 			var feelsLike3 = data.list[24].main.feels_like.toFixed(0);
 			var date3 = data.list[24].dt_txt;
 
-			$('#date3').html(date3);
-			$('#desc3').append(dayDesc3);
-			$('#temp3').html(dayTemp3 + '<sup>°c</sup>');
-			$('#icon3').empty().append(image3);
-			$('#feels3').html('Feels like: ' + feelsLike3 + '<sup>°c</sup>');
-
 
 			/*Day 4 Forecast*/
 			var dayTemp4 = data.list[32].main.temp.toFixed(0);
@@ -100,30 +75,85 @@ function load(){
 			var feelsLike4 = data.list[32].main.feels_like.toFixed(0);
 			var date4 = data.list[32].dt_txt;
 
-			$('#date4').html(date4);
-			$('#desc4').append(dayDesc4);
-			$('#temp4').html(dayTemp4 + '<sup>°c</sup>');
-			$('#icon4').empty().append(image4);
-			$('#feels4').html('Feels like: ' + feelsLike4 + '<sup>°c</sup>');
-
 
 			/*Day 5 Forecast*/
-			var dayTemp5 = data.list[40].main.temp.toFixed(0);
-			var dayDesc5 = data.list[40].weather[0].description;
-			var feelsLike5 = data.list[40].main.feels_like.toFixed(0);
-			var date5 = data.list[40].dt_txt;
-
-			$('#date5').html(date5);
-			$('#desc5').append(dayDesc5);
-			$('#temp5').html(dayTemp5 + '<sup>°c</sup>');
-			$('#icon5').empty().append(image5);
-			$('#feels5').html('Feels like: ' + feelsLike5 + '<sup>°c</sup>');
-
-
 			
-		 
+
+
+
+			const li = document.createElement("li");
+      		li.classList.add("city");
+			 const markup = `
+			 <div class="container">
+			 <div class="row">
+			   <div class="col-sm" id="day">
+				 <div class="card" style="width: 18rem; background-color: #f8f8f8; border-color: #f8f8f8;">
+					 <div class="card-body">
+					   <h5 class="card-title" id="date">Current Weather</h5>
+					   <h5 class="card-title" id="city-name">${cityName}, ${country}</h5>
+					   <h6 class="card-subtitle mb-2 text-muted" id="desc">${dayDesc}</h6>
+					   <h1 class="card-text" id="city-temp">${dayTemp}<sup>°c</sup></h1>
+					   <figure> <img class="city-icon" src="${image.src}"></figure>
+					   <h6 class="card-link" id="feels">Feels like: ${feelsLike}<sup>°c</sup></h6>
+					 </div>
+				   </div>
+			   </div>
+			   <div class="col-sm" id="day">
+				 <div class="card" style="width: 18rem; background-color: #f8f8f8; border-color: #f8f8f8;">
+					 <div class="card-body">
+					 <h5 class="card-title" id="date1">${date1}</h5>
+					 <h6 class="card-subtitle mb-2 text-muted" id="desc">${dayDesc1}</h6>
+					 <h1 class="card-text" id="city-temp1">${dayTemp1}<sup>°c</sup></h1>
+					 <figure> <img class="city-icon" src="${image1.src}"></figure>
+					 <h6 class="card-link" id="feels">Feels like: ${feelsLike1}<sup>°c</sup></h6>
+					 </div>
+				   </div>
+			   </div>
+			   <div class="col-sm" id="day">
+				 <div class="card" style="width: 18rem; background-color: #f8f8f8; border-color: #f8f8f8;">
+					 <div class="card-body">
+					 <h5 class="card-title" id="date2">${date2}</h5>
+					 <h6 class="card-subtitle mb-2 text-muted" id="desc">${dayDesc2}</h6>
+					 <h1 class="card-text" id="city-temp2">${dayTemp2}<sup>°c</sup></h1>
+					 <figure> <img class="city-icon" src="${image2.src}"></figure>
+					 <h6 class="card-link" id="feels">Feels like: ${feelsLike2}<sup>°c</sup></h6>
+					 </div>
+				   </div>
+			   </div>
+			 </div>
+			 <div class="row">
+			   <div class="col-sm" id="day">
+				 <div class="card" style="width: 18rem; background-color: #f8f8f8; border-color: #f8f8f8;">
+					 <div class="card-body">
+					   <h5 class="card-title" id="date">${date3}</h5>
+					   <h6 class="card-subtitle mb-2 text-muted" id="desc">${dayDesc3}</h6>
+					   <h1 class="card-text" id="city-temp">${dayTemp3}<sup>°c</sup></h1>
+					   <figure> <img class="city-icon" src="${image3.src}"></figure>
+					   <h6 class="card-link" id="feels">Feels like: ${feelsLike3}<sup>°c</sup></h6>
+					 </div>
+				   </div>
+			   </div>
+			   <div class="col-sm" id="day">
+				 <div class="card" style="width: 18rem; background-color: #f8f8f8; border-color: #f8f8f8;">
+					 <div class="card-body">
+					 <h5 class="card-title" id="date1">${date4}</h5>
+					 <h6 class="card-subtitle mb-2 text-muted" id="desc">${dayDesc4}</h6>
+					 <h1 class="card-text" id="city-temp1">${dayTemp4}<sup>°c</sup></h1>
+					 <figure> <img class="city-icon" src="${image4.src}"></figure>
+					 <h6 class="card-link" id="feels">Feels like: ${feelsLike4}<sup>°c</sup></h6>
+					 </div>
+				   </div>
+			   </div>
+			   
+			   </div>
+			 </div>
+		   </div> `;
+		   li.innerHTML = markup;
+      		list.appendChild(li);
 			
 		}
+
+
 	})
 	.catch(() => {
 		msg.textContent = "Please search for a valid city";
